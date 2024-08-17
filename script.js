710be938441c9a94d3dd5421aede9966858a6276
Alton <smithiialton@gmail.com>
2024-08-17 14:23:55 -0700

    completed problem 1

---
+++ script.js
@@ -0,0 +1,44 @@
+/* Problem Statement: Imagine you are developing a student management system for a school. Your task is to create 
+a JavaScript program that declares variables to store information about students, such as their names, ages, grades, 
+and attendance status.
+
+Task 1: Declare variables to represent student information such as name, age, grade, and attendance status. */
+
+let student_name = "John Doe"
+let age = 17
+let grade = "12th"
+let attendance_status = false
+
+console.log("Task 1:",student_name)
+console.log("Task 1:",age)
+console.log("Task 1:",grade)
+console.log("Task 1:",attendance_status)
+// Task 2: Assign sample values to the student information variables.
+
+// Task 2: Assign sample values
+// studentName = "Alice";
+// studentAge = 15;
+// studentGrade = 10;
+// isPresent = true;
+
+student_name = "Alice"
+age = 15
+grade ="10th"
+attendance_status = true
+
+
+// Task 3: Display the student information using console.log() statements.
+
+// Expected Outcomes:
+
+console.log("Task 2:",student_name)
+console.log("Task 2:",age)
+console.log("Task 2:",grade)
+console.log("Task 2:",attendance_status)
+
+
+
+
+// After completing Task 1, students should understand how to declare variables with different data types in JavaScript.
+// After completing Task 2, students should be able to assign values to variables of various data types.
+// After completing Task 3, students should be able to display variable values using console.log() statements.
